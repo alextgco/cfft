1b3d3dd86c2a4c5d02539e4d81f279fa8634835c
alextgco <alextgco@gmail.com>
2015-02-13 00:29:51 +0300

    не важно

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 var nconf = require('nconf');
 var path = require('path');
 var filename = 'config.json';
-var remote = false;
+var remote = true;
 if (remote){
     filename = 'configREMOTE.json';
 }
+console.log('Config selection.', filename);
 nconf.argv()
     .env()
     .file({file:path.join(__dirname,filename)});
