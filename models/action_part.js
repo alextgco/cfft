814abf80c3876916d026ff291a5318646e317ef5
alextgco <alextgco@gmail.com>
2015-02-21 22:09:44 +0300

    MySQLModel.js

--- models/action_part.js
+++ models/action_part.js
@@ -1,19 +1,29 @@
 var Model = require('./MySQLModel');
-var action = new Model({
-    table: 'action_parts',
-    table_ru: 'Этап мероприятия',
-    ending:'',
-    required_fields:['payment_type_id','title'],
-    blob_fields:['description'],
-    join_objs:[
-        {
-            action_id:{
-                table:"actions",
-                fields:[]
+module.exports = function(callback){
+    var action = new Model({
+        table: 'action_part',
+        table_ru: 'Этап мероприятия',
+        ending:'',
+        required_fields:['payment_type_id','title'],
+        blob_fields:['description1','description2'],
+        join_objs:[
+            {
+                payment_type_id:{
+                    table:"action_payment_types",
+                    fields:[
+                        {
+                            column:"name",
+                            alias:"payment_type"
+                        }
+                    ]
+                }
             }
+        ]
+    },function(err){
+        if (err){
+            console.log(err);
         }
-    ]
-},function(){
-    module.exports = action;
-});
+        callback(action);
+    });
+};
 
--- routes/admin_event.js
+++ routes/admin_event.js
@@ -1,25 +1,35 @@
+var async = require('async');
 var api = require('../libs/api');
 exports.get = function(req, res, next){
     var id = req.query.id;
-    api('get', 'action_rewards', {where: {action_id: id}},function(err,result){
+
+    var loadActionRewards = function(callback){
+        api('get', 'action_rewards', {where: {action_id: id}},function(err,result){
+            if (err){
+                console.log(err, 'action_rewards');
+                return callback(err);
+            }
+            callback(null,result);
+        });
+    };
+    var getAction = function(callback){
+        api('get', 'action', {where: {id: id}},function(err,result){
+            if (err){
+                console.log(err, 'admin_event');
+                return callback(err);
+            }
+            callback(null,result);
+        });
+    };
+    async.series([
+        loadActionRewards,
+        getAction
+    ],function(err,results){
         if (err){
-            console.log(err, 'action_rewards');
-        }else{
-            console.log('action_rewards',result);
-            api('get', 'action', {where: {id: id}},function(err,result){
-                if (err){
-                    console.log(err, 'admin_event');
-                }else{
-                    console.log(result);
-                    res.render('admin_event', {
-                        data: result
-                    });
-                }
-
-            });
-
+            return next(err);
         }
-
+        res.render('admin_event', {
+            data: results[1]
+        });
     });
-
 };
