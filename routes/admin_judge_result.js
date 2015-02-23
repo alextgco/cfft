ea9f51f64e1a6ef07dacfd7a8678059fd632a519
alextgco <alextgco@gmail.com>
2015-02-24 02:43:48 +0400

    event appeared!

--- routes/admin_judge_result.js
+++ routes/admin_judge_result.js
@@ -1,11 +1,12 @@
 var api = require('../libs/api');
 exports.get = function(req, res, next){
-    api('get', 'results', {}, function(err,result){
+    api('get', 'results', {where: {id: id}}, function(err,result){
+        var id = req.query.id;
         if (err){
-            console.log(err, 'admin_results');
+            console.log(err, 'admin_judge_result');
         }else{
             console.log(result);
-            res.render('admin_results', {
+            res.render('admin_judge_result', {
                 items: result
             });
         }
