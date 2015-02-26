6d3df4f1aaf30b065db2dc8d3854f4e1aad8e7da
alextgco <alextgco@gmail.com>
2015-02-26 22:57:29 +0400

    easdfsdf

--- routes/wows.js
+++ routes/wows.js
@@ -23,7 +23,7 @@ exports.get = function(req, res, next){
                 var m = s_date.substr(5,2);
                 var d = s_date.substr(8,2);
                 var textDate = (s_date.length > 0 && s_date != 'null')? d +' '+ mths[parseInt(m)-1] + ' ' + y : '';
-                result.data[i]['date_start_text'] = textDate;
+                result.data[i]['date_start_text'] = (s_date.length > 0)? textDate : '';
             }
 
             res.render('wows', {
