95d26b249dcba0de5183b01d0f12b41340d39cf6
alextgco <alextgco@gmail.com>
2015-02-28 22:10:40 +0300

    MySQLModel.js

---
+++ models/city.js
@@ -0,0 +1,41 @@
+var Model = require('./MySQLModel');
+var MyError = require('../error').MyError;
+var funcs = require('../libs/functions');
+module.exports = function(callback){
+    var city = new Model({
+        table: 'cities',
+        table_ru: 'Город',
+        ending:'',
+        required_fields:['country_id','title'],
+        validation: {
+            country_id:'number',
+            title:'notNull'
+        },
+        getFormating:{
+
+        },
+        join_objs:[
+            {
+                country_id:{
+                    table:"countries",
+                    fields:[
+                        {
+                            column:"id",
+                            alias:"country_id"
+                        },
+                        {
+                            column:"title",
+                            alias:"country"
+                        }
+                    ]
+                }
+            }
+        ]
+    },function(err){
+        if (err){
+            console.log(err);
+        }
+        callback(city);
+    });
+};
+
