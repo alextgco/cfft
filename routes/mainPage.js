fb83583cf45949393d211272ae69c9bc9f877e23
alextgco <alextgco@gmail.com>
2015-03-18 00:43:04 +0400

    event appeared!

--- routes/mainPage.js
+++ routes/mainPage.js
@@ -1,6 +1,6 @@
 var api = require('../libs/userApi');
 exports.get = function(req, res, next){
-    api('get', 'article', {where: {published: true}}, function(err,result){
+    api('get', 'article', function(err,result){
         if (err){
             console.log(err, 'mainPage');
         }else{
