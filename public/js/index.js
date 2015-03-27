28c94d6e1bfcc3860e6e6a6c1d0ec558d1b0dbab
alextgco <alextgco@gmail.com>
2015-03-27 22:11:43 +0400

    event appeared!

--- public/js/index.js
+++ public/js/index.js
@@ -854,6 +854,7 @@ $(document).ready(function(){
         //console.log(y, m-1, d, h, min, new Date(y, m-1, d, h, min));
 
         if(new Date(y,m-1,d) < new Date()){
+            $('.nearresr-wow-timer-label').html('');
             $('.nearresr-wow-timer-init').html('<div class="white">Wod of week проходит прямо сейчас, спешите принять участие!</div>');
             $('.t_tblc').remove();
         }else{
