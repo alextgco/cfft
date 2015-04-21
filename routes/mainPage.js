c980c033d42b1a8df4302e2d319df799e522d29a
alextgco <alextgco@gmail.com>
2015-04-22 01:35:33 +0400

    event appeared!

--- routes/mainPage.js
+++ routes/mainPage.js
@@ -1,13 +1,50 @@
 var api = require('../libs/userApi');
+var async = require('async');
 exports.get = function(req, res, next){
-    api('get', 'article', {limit:3}, function(err,result){
+
+    var loadArticles = function(callback){
+        api('get', 'article', {limit:3}, function(err,result){
+            if (err){
+                console.log(err, 'mainPage');
+            }else{
+                console.log('ART', result);
+                //res.render('mainPage', {
+                //    data: result
+                //});
+            }
+
+            callback(null,result);
+        });
+    };
+
+    var loadSlides = function(callback){
+        api('get', 'main_slides', {limit:3}, function(err,result){
+            if (err){
+                console.log(err, 'main_slides');
+            }else{
+                console.log('ART', result);
+                //res.render('mainPage', {
+                //    data: result
+                //});
+            }
+
+            callback(null,result);
+        });
+    };
+
+
+    async.series([
+        loadArticles,
+        loadSlides
+    ],function(err,results){
         if (err){
-            console.log(err, 'mainPage');
-        }else{
-            console.log('ART', result);
-            res.render('mainPage', {
-                data: result
-            });
+            return next(err);
         }
+        res.render('mainPage', {
+            data: {
+                data: results[0],
+                slides: results[1]
+            }
+        });
     });
 };
