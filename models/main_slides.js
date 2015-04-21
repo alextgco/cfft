1183ac4235219eb69a571416e94d1946b6e8fb5b
igoptarev <ivantgco@gmail.com>
2015-04-22 00:58:22 +0300

    Добавил сущность main_slides

--- models/main_slides.js
+++ models/main_slides.js
@@ -3,9 +3,9 @@ var MyError = require('../error').MyError;
 var funcs = require('../libs/functions');
 module.exports = function(callback){
     var main_slides = new Model({
-        table: 'articles',
-        table_ru: 'Статья',
-        ending:'а',
+        table: 'main_slides',
+        table_ru: 'Слайд',
+        ending:'',
         published:false,
         required_fields:['photo','sort_no'],
         validation: {
